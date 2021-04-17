026cbdee834272d523812b659da1bc465f505870
Brice Friha <Brice.friha@outlook.com>
2021-04-17 18:19:22 +0200

    Android: scroll to the top when a tab is reselected

---
+++ AresNews/AresNews/Models/MessageItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AresNews.Models
+{
+    public class MessageItem
+    {
+        public Guid Id { get; set; }
+    }
+}
