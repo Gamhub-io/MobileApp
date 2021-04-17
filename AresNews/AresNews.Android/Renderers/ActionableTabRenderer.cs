9526ba4d7a8acb5e72499b28937ec42fd4c48d48
Brice Friha <Brice.friha@outlook.com>
2021-04-17 17:25:05 +0200

    Create a shell rendrer

---
+++ AresNews/AresNews.Android/Renderers/ActionableTabRenderer.cs
@@ -0,0 +1,24 @@
+﻿
+using Android.Content;
+using AresNews.Controls;
+using AresNews.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Shell), typeof(ActionableTabRenderer))]
+
+namespace AresNews.Droid.Renderers
+{
+    public class ActionableTabRenderer : ShellRenderer
+    {
+        public ActionableTabRenderer(Context context) : base(context)
+        {
+        }
+        protected override IShellItemRenderer CreateShellItemRenderer(ShellItem shellItem)
+        {
+            return new ActionableTabItemRenderer(this);
+        }
+
+    }
+    
+}
