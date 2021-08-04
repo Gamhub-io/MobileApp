1f4bbbdcf875c5d1183b04463d8e951a7206a908
Brice Friha <Brice.friha@outlook.com>
2021-08-04 18:55:42 +0200

    Reajust convertion time

--- AresNews/AresNews/Models/Article.cs
+++ AresNews/AresNews/Models/Article.cs
@@ -20,6 +20,7 @@ namespace AresNews.Models
         public string Author { get; set; }
         [JsonProperty("image")]
         public string Image { get; set; }
+        [JsonProperty("isoDate")]
         public DateTime FullPublishDate { get; set; }
         public string PublishDate { get 
             {
