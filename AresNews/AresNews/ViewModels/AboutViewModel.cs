1679ea6a083dd451ca22919e1a453f2db6c0eb14
Brice Friha <Brice.friha@outlook.com>
2021-10-12 19:39:18 +0100

    Add repository link

---
+++ AresNews/AresNews/ViewModels/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AresNews.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+        }
+
+        public Command ViewGithub
+        {
+            get
+            {
+                return new Command(async () => await Browser.OpenAsync("https://github.com/bricefriha/AresGaming", new BrowserLaunchOptions
+                {
+                    LaunchMode = BrowserLaunchMode.External,
+                    TitleMode = BrowserTitleMode.Default,
+                }));
+            }
+        }
+    }
+}
--- AresNews/AresNews/Views/AboutPage.xaml.cs
+++ AresNews/AresNews/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AresNews.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,8 @@ namespace AresNews.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            BindingContext = new AboutViewModel();
         }
     }
 }
