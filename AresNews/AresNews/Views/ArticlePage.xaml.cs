29a36baeff865fb8acf9e1b61241fd509ee3c5f9
Brice Friha <Brice.friha@outlook.com>
2022-04-25 14:01:31 +0100

    Article page: Publish date grid columspan

--- AresNews/AresNews/Views/ArticlePage.xaml.cs
+++ AresNews/AresNews/Views/ArticlePage.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using AresNews.Models;
 using AresNews.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
