883816c7150dda778584cd03901a0ead856a872c
Brice Friha <Brice.friha@outlook.com>
2022-08-28 12:14:34 +0100

    Add a "support" button in the flyout that mailto the support email address

---
+++ AresNews/AresNews/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AresNews.ViewModels
+{
+    public class AppShellViewModel : BaseViewModel
+    {
+        public Command MailTo
+        {
+            get
+            {
+                return new Command<string>(async (address) => await Email.ComposeAsync(subject: "", body: "", to: new string[] { address }));
+            }
+        }
+    }
+}
