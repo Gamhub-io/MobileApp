dde2ac2e05f382f9f2e1c37c09d1ed61c2e7bec2
Brice Friha <Brice.friha@outlook.com>
2023-04-09 19:03:59 +0100

    Fixed issue on loading main feed

--- AresNews/AresNews/Models/Source.cs
+++ AresNews/AresNews/Models/Source.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.ObjectModel;
 
 namespace AresNews.Models
 {
@@ -7,6 +10,8 @@ namespace AresNews.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [Column("mongoId"), JsonProperty("_id")]
+        public string MongoId { get; set; }
         [JsonProperty("name")]
         public string Name { get; set; }
         [JsonProperty("url")]
@@ -19,5 +24,7 @@ namespace AresNews.Models
         public string Domain { get; set; }
         [JsonProperty("isActive")]
         public bool IsActive { get; set; }
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public Collection<Article> RelatedArticles { get; set; }
     }
 }
