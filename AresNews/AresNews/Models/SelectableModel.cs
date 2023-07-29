cbbe1937fcab9d255cac4633954c8d87c45e45f3
Brice Friha <Brice.friha@outlook.com>
2023-07-29 15:02:00 +0100

    Feeds - Simplifying complexity of the code for feed selection

---
+++ AresNews/AresNews/Models/SelectableModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AresNews.Models
+{
+    public class SelectableModel : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                //if (_isSelected != value)
+                //{
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                //}
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
