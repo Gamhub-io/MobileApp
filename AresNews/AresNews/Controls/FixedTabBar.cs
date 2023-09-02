e410ef8363e82deda77dd16c87140aceb00687ab
Brice Friha <Brice.friha@outlook.com>
2023-09-02 20:17:25 +0100

    Fixing the fucking TABBAR ISSUE

---
+++ AresNews/AresNews/Controls/FixedTabBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AresNews.Controls
+{
+    public class FixedTabBar : TabBar
+    {
+
+    }
+}
