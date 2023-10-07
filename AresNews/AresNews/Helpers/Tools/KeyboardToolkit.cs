236dcbfcc873facbaf945581db0e2f6e473aa05a
Brice Friha <Brice.friha@outlook.com>
2023-10-07 10:03:00 +0100

    Hide keyboard when back from search

---
+++ AresNews/AresNews/Helpers/Tools/KeyboardToolkit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AresNews.Helpers.Tools
+{
+    public class KeyboardToolkit
+    {
+        public void HideKeyBoard()
+        {
+            DependencyService.Get<IKeyboardHelper>().HideKeyboard();
+        }
+    }
+}
