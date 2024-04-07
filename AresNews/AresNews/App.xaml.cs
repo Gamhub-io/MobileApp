b69d0b9d47dc359bff86497b76abe5bc4d7805cc
bricefriha <brice.friha@outlook.com>
2024-04-07 12:11:49 +0100

    Discord signin - Popup to signin on discord

--- AresNews/AresNews/App.xaml.cs
+++ AresNews/AresNews/App.xaml.cs
@@ -20,12 +20,6 @@ using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: ExportFont("FontAwesome6Free-Regular-400.otf", Alias = "FaRegular")]
-[assembly: ExportFont("FontAwesome6Brands-Regular-400.otf", Alias = "FaBrand")]
-[assembly: ExportFont("FontAwesome6Free-Solid-900.otf", Alias = "FaSolid")]
-[assembly: ExportFont("Ubuntu-Regular.ttf", Alias = "P-Regular")]
-[assembly: ExportFont("Ubuntu-Bold.ttf", Alias = "P-Bold")]
-[assembly: ExportFont("Ubuntu-Medium.otf", Alias = "P-SemiBold")]
 namespace AresNews
 {
     public partial class App : Application
---
+++ AresNews/AresNews/Views/PopUps/AuthPopUp.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms.Xaml;
+
+namespace AresNews.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AuthPopUp : PopupPage
+    {
+		public AuthPopUp()
+		{
+			InitializeComponent ();
+		}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // prevent the page from collapsing when the keyboard appears
+            HasSystemPadding = false;
+
+
+        }
+
+        private void Discord_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
