0e9d4bade50d20e8fa9638aa71b10bd071c07e81
bricefriha <brice.friha@outlook.com>
2024-04-07 12:11:49 +0100

    Discord signin - Popup to signin on discord

--- AresNews/AresNews/ViewModels/AppShellViewModel.cs
+++ AresNews/AresNews/ViewModels/AppShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MvvmHelpers;
+﻿using AresNews.Views;
+using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,8 @@ namespace AresNews.ViewModels
 {
     public class AppShellViewModel : BaseViewModel
     {
+        private int logoClickCount;
+
         public Command MailTo
         {
             get
@@ -16,5 +19,34 @@ namespace AresNews.ViewModels
                 return new Command<string>(async (address) => await Email.ComposeAsync(subject: "", body: "", to: new string[] { address }));
             }
         }
+        public Command OpenAuth
+        {
+            get
+            {
+                return new Command( () =>
+                {
+                    logoClickCount++;
+
+                    if (logoClickCount == 3)
+                    {
+                        // Reset the logo click count
+                        logoClickCount = 0;
+
+                        // Open the login pop up
+                        CurrenApp.OpenPopUp(new AuthPopUp());
+                    }
+
+
+                });
+            }
+        }
+
+        public App CurrenApp { get; private set; }
+
+        public AppShellViewModel()
+        {
+            CurrenApp = (App)App.Current;
+        }
+
     }
 }
