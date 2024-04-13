3495f65c310c4da103a6ad588e01a3157685f75f
bricefriha <brice.friha@outlook.com>
2024-04-13 18:58:46 +0100

    Add app settings

---
+++ AresNews/AresNews/Core/AppConstant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AresNews.Core
+{
+    public static class AppConstant
+    {
+        public static string DiscordClientId = Environment.GetEnvironmentVariable("discord_client_id");
+        public static string ApiHost = Environment.GetEnvironmentVariable("api_host");
+    }
+}
