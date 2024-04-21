91bec0a061579f483f10df647e2264655562388e
bricefriha <brice.friha@outlook.com>
2024-04-21 19:00:58 +0100

    Discord auth - Navigate back after login

--- AresNews/AresNews/Views/Portals/DiscordAuthPortal.xaml.cs
+++ AresNews/AresNews/Views/Portals/DiscordAuthPortal.xaml.cs
@@ -21,5 +21,24 @@ namespace AresNews.Views.Portals
             Url = $"https://discord.com/oauth2/authorize?client_id={AppConstant.DiscordClientId}&response_type=code&redirect_uri=https%3A%2F%2F{AppConstant.ApiHost}%2Fauth%2Fdiscord&scope=email+identify+connections";
             BindingContext = this;
         }
+
+        private void DiscordPortal_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            // Catch the navigation to the api
+            if (e.Url.Contains($"//{AppConstant.ApiHost}/auth/discord"))
+            {
+                // Navigate back
+                (App.Current as App).MainPage.Navigation.RemovePage(this);
+            }
+        }
+
+        private void DiscordPortal_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            // Catch the navigation to the api
+            if (e.Url.Contains($"//{AppConstant.ApiHost}/auth/discord"))
+            {
+                DiscordPortal.IsVisible = false;
+            }
+        }
     }
 }
