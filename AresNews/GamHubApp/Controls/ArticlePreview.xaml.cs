09d6ba67491b4c4cee27843bfa0d3badc04fe470
bricefriha <contact@bricefriha.dev>
2024-07-14 16:10:52 +0100

    #111 - MAUI Migration |  update Community tool kit

--- AresNews/GamHubApp/Controls/ArticlePreview.xaml.cs
+++ AresNews/GamHubApp/Controls/ArticlePreview.xaml.cs
@@ -1,13 +1,4 @@
 ﻿using GamHub.Models;
-using FFImageLoading;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui;
 
 namespace GamHub.Controls
 {
