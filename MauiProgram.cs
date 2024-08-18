5bf3a0f6c3c2d11f8a77111e6c8c665443c4251a
bricefriha <contact@bricefriha.dev>
2024-08-18 14:38:41 +0100

    MAUI - resources | import fonts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -17,6 +17,12 @@ namespace Gamhub
                        fonts.AddFont("ComicShark.otf", "ComicShark");
                        fonts.AddFont("SonicComics.ttf", "SonicComics");
                        fonts.AddFont("MouseMemoirs-Regular.ttf", "MouseMemoirs-Regular");
+                       fonts.AddFont("FontAwesome6Free-Regular-400.otf", "FaRegular");
+                       fonts.AddFont("FontAwesome6Brands-Regular-400.otf", "FaBrand");
+                       fonts.AddFont("FontAwesome6Free-Solid-900.otf", "FaSolid");
+                       fonts.AddFont("Ubuntu-Regular.ttf", "P-Regular");
+                       fonts.AddFont("Ubuntu-Bold.ttf", "P-Bold");
+                       fonts.AddFont("Ubuntu-Medium.ttf", "P-Medium");
                    }).UseMauiCommunityToolkit();
 #if DEBUG
             //builder.Logging.AddDebug();
