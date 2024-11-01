46390f517f05420f5c5fa86b1018af04aa0acc78
bricefriha <contact@bricefriha.dev>
2024-11-01 10:29:37 +0000

    Setup - config | add setup to debug locally

--- Core/AppConstant.cs
+++ Core/AppConstant.cs
@@ -7,5 +7,6 @@ namespace GamHubApp.Core
         public static string DiscordClientId = Environment.GetEnvironmentVariable("discord_client_id");
         public static string ApiHost = Environment.GetEnvironmentVariable("api_host");
         public static string MonitoringKey = Environment.GetEnvironmentVariable("monitoring_key");
+        public static string Localhost = Environment.GetEnvironmentVariable("localhost");
     }
 }
