b71365cccc268454b65b33b3b04fd49d53fa60ec
bricefriha <contact@bricefriha.dev>
2025-01-24 18:14:58 +0000

    navigation - Article view | fix bug back navigation closing the app

---
+++ App/Platforms/Android/BackPress.cs
@@ -0,0 +1,58 @@
+﻿using AndroidX.Activity;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace GamHubApp.Platforms.Android;
+
+internal class BackPress : OnBackPressedCallback
+{
+    public BackPress() : base(true)
+    {
+
+    }
+
+    public override void HandleOnBackPressed()
+    {
+        if (Platform.CurrentActivity is not null
+            && IPlatformApplication.Current?.Services is not null)
+        {
+            InvokeLifecycleEvents<AndroidLifecycle.OnBackPressed>(IPlatformApplication.Current.Services, del =>
+            {
+                del(Platform.CurrentActivity);
+            });
+        }
+    }
+
+    internal static void InvokeLifecycleEvents<TDelegate>(IServiceProvider services, Action<TDelegate> action)
+        where TDelegate : Delegate
+    {
+        if (services == null)
+        {
+            return;
+        }
+
+        var delegates = GetLifecycleEventDelegates<TDelegate>(services);
+
+        foreach (var del in delegates)
+        {
+            action?.Invoke(del);
+        }
+    }
+
+    internal static IEnumerable<TDelegate> GetLifecycleEventDelegates<TDelegate>(IServiceProvider? services, string? eventName = null)
+        where TDelegate : Delegate
+    {
+        var lifecycleService = services?.GetService<ILifecycleEventService>();
+
+        if (lifecycleService == null)
+        {
+            yield break;
+        }
+
+        eventName ??= typeof(TDelegate).Name;
+
+        foreach (var del in lifecycleService.GetEventDelegates<TDelegate>(eventName))
+        {
+            yield return del;
+        }
+    }
+}
--- App/Platforms/Android/MainActivity.cs
+++ App/Platforms/Android/MainActivity.cs
@@ -1,9 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using GamHubApp.Platforms.Android;
 
 namespace GamHubApp;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        OnBackPressedDispatcher.AddCallback(this, new BackPress());
+    }
 }
