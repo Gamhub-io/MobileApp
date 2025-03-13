806cfff7c7cb88e70382162c34f50db7f30e0ed0
bricefriha <contact@bricefriha.dev>
2025-03-13 11:03:42 +0000

    Code base - Org | Organise the weak reference messages classes

---
+++ App/Services/ChangedMessages/ScrollMainPageChangedMessage.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using GamHubApp.ViewModels;
+using GamHubApp.Views;
+
+namespace GamHubApp.Services.ChangedMessages;
+
+public class ScrollMainPageChangedMessage : ValueChangedMessage<BaseViewModel>
+{
+    public ScrollMainPageChangedMessage(BaseViewModel value) : base(value)
+    {
+    }
+}
