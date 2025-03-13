15b5c31c883b0e0312e45c81564bf20cc42a5265
bricefriha <contact@bricefriha.dev>
2025-03-13 11:03:42 +0000

    Code base - Org | Organise the weak reference messages classes

---
+++ App/Services/ChangedMessages/UnnoticedArticlesChangedMessage.cs
@@ -0,0 +1,17 @@
+﻿
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using GamHubApp.Models;
+using System.Collections.ObjectModel;
+
+namespace GamHubApp.Services;
+
+public class UnnoticedArticlesChangedMessage : ValueChangedMessage<ObservableCollection<Article>>
+{
+    public UnnoticedArticlesChangedMessage(ObservableCollection<Article> value) : base(value)
+    {
+        Count = value.Count;
+    }
+
+    public int Count { get; set; }
+    public DateTime UpdateTime { get; private set; } = DateTime.Now;
+}
