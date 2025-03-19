68d6eb30c3e707b2b0d204e6b14b8d35fd09e635
bricefriha <contact@bricefriha.dev>
2025-03-19 09:04:54 +0000

    Fix - Feeds VM | make sure to manage `IsBusy` properly inside `UpdateFeeds()`

--- App/ViewModels/FeedsViewModel.cs
+++ App/ViewModels/FeedsViewModel.cs
@@ -359,10 +359,9 @@ public class FeedsViewModel : BaseViewModel
             {
                 // End the loading indicator
                 IsRefreshing = false;
-                IsBusy = false;
             } }
             
-        );
+        ).ContinueWith ((tr) => IsBusy = false);
         
     }
 
@@ -651,6 +650,7 @@ public class FeedsViewModel : BaseViewModel
     {
         if (!_dataLoaded) return;
         if (_feeds == null) return;
+        IsBusy = true;
 
         Collection<Feed> updatedFeeds;
         using (var conn = new SQLiteConnection(App.GeneralDBpath))
@@ -661,7 +661,10 @@ public class FeedsViewModel : BaseViewModel
         }
 
         if (updatedFeeds is null)
+        {
+            IsBusy = false;
             return;
+        }
 
         List<Feed> newFeeds = updatedFeeds.Where(feed => !_feeds.Any(item => item.Id == feed.Id)).ToList();
         List<Feed> removedFeeds = _feeds.Where(feed => !updatedFeeds.Any(item => item.Id == feed.Id)).ToList();
@@ -676,6 +679,7 @@ public class FeedsViewModel : BaseViewModel
         {
             _ =RemoveFeed(feed);
         }
+        IsBusy = false;
     }
 
     /// <summary>
