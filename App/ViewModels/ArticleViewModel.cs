0a633ff0a549cbd9c193ebf0257d54c2d6fbe9db
bricefriha <contact@bricefriha.dev>
2025-05-04 13:31:24 +0100

    ArticleViewModel - constructor | check `Deals` before treating it

--- App/ViewModels/ArticleViewModel.cs
+++ App/ViewModels/ArticleViewModel.cs
@@ -181,17 +181,18 @@ public class ArticleViewModel : BaseViewModel
     {
         try
         {
-            if (_dealEnabled = Preferences.Get(AppConstant.DealArticleEnable, true))
-                Deals = new ObservableCollection<Deal>((App.Current as App).Deals?.Where(deal => 
+            Collection<Deal> deals = (App.Current as App).Deals;
+            if (_dealEnabled = Preferences.Get(AppConstant.DealArticleEnable, true)
+                && deals is not null)
+                Deals = new ObservableCollection<Deal>(deals.Where(deal =>
                 {
-                    for (int i = 0; i < article.Categories?.Count(); i++) 
-                        if (deal.Title.ToLower().Contains(article.Categories[i].ToLower()) 
+                    for (int i = 0; i < article.Categories?.Count(); i++)
+                        if (deal.Title.ToLower().Contains(article.Categories[i].ToLower())
                         ||
                         deal.Description.ToLower().Contains(article.Categories[i].ToLower()))
                             return true;
                     return false;
                 }).ToList());
-
         }
         catch (Exception ex)
         {
