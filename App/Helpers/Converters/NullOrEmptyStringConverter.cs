9e1f59e69b5abacdae7c31be2f82c2a7cb978435
bricefriha <contact@bricefriha.dev>
2025-05-13 12:32:11 +0100

    UI - Article Preview | display the logo of the source if available

---
+++ App/Helpers/Converters/NullOrEmptyStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace GamHubApp.Helpers;
+
+class NullOrEmptyStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string val)
+        {
+            return string.IsNullOrEmpty(val);
+        }
+        return true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
