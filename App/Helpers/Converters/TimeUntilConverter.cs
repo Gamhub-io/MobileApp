0f3b19345b3332d89fd128d622bdea4673c80904
Brice Friha <37577669+bricefriha@users.noreply.github.com>
2025-05-15 16:24:41 +0100

    feat: deals - display time remaining until deals expire (#268)
    * Converters - | create a time until converter
    
    * Deals - page | display time until it expires
    
    * Deals page | improving time until display
    
    * TimeUntilConverter - correction
    
    * TimeUntilConverter - converters | improve
    
    * Deal page - expiration | dynamically change the colour
    
    * Converters - time until| correct a big mistake
    
    * App -| make sure to refresh the Deals when resuming the app
    
    * converters - TimeUntil | remove minutes when it's not relevant
    
    Resolves #260

---
+++ App/Helpers/Converters/TimeUntilConverter.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Globalization;
+
+namespace GamHubApp.Helpers;
+
+public class TimeUntilConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime enddate) 
+        {
+            var timeremaining = enddate - DateTime.Now;
+
+            if (timeremaining < TimeSpan.Zero)
+                return "expired";
+            else if (timeremaining.TotalHours < 0)
+                return $"{timeremaining.Minutes} minutes";
+            else if (timeremaining.TotalDays < 0)
+                return $"{timeremaining.Days} hours {timeremaining.Minutes} minutes ";
+            else if (timeremaining.TotalDays < 30)
+                return $"{timeremaining.Days} days {timeremaining.Hours} hours";
+
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
