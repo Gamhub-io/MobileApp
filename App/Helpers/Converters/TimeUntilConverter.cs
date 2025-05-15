32eb84ed29ffc9a11d38a331642c5bbbb9ae440c
bricefriha <contact@bricefriha.dev>
2025-05-15 14:49:33 +0100

    Converters - | create a time until converter

---
+++ App/Helpers/Converters/TimeUntilConverter.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Globalization;
+
+namespace GamHubApp.Helpers;
+
+public class TimeUntilConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime enddate) 
+        {
+            var timeremaining = enddate - DateTime.Now;
+
+            if (timeremaining < TimeSpan.Zero)
+                return "expired";
+            else if (timeremaining < new TimeSpan(days: 30, 0, 0, 0))
+                return $"Ends in {timeremaining.Days}d {timeremaining.Days}hrs {timeremaining.Minutes}mins";
+
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
