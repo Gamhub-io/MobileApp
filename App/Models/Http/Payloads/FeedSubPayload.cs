bb1b275a9df1da662ed55505db0d3d18a7adff3d
bricefriha <contact@bricefriha.dev>
2025-05-20 19:51:46 +0100

    feed sub payload fix syntax

--- App/Models/Http/Payloads/FeedSubPayload.cs
+++ App/Models/Http/Payloads/FeedSubPayload.cs
@@ -5,7 +5,7 @@ namespace GamHubApp.Models.Http.Payloads;
 public sealed class FeedSubPayload
 {
     [JsonProperty("token")]
-    public string Token { get; set; }]
+    public string Token { get; set; }
 
     [JsonProperty("feed")]
     public string Feed { get; set; }
