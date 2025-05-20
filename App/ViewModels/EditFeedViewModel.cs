563d26b402e54291e5915ad85e2b85b7e3813449
bricefriha <contact@bricefriha.dev>
2025-05-20 19:02:02 +0100

    Edit feed

--- App/ViewModels/EditFeedViewModel.cs
+++ App/ViewModels/EditFeedViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
+using GamHubApp.Core;
 using GamHubApp.Models;
 using GamHubApp.Services;
 using GamHubApp.Views;
@@ -20,6 +21,18 @@ public class EditFeedViewModel : BaseViewModel
             OnPropertyChanged(nameof(Feed));
         }
     }
+
+    private bool _feedNotification;
+    public bool FeedNotification
+    {
+        get { return _feedNotification; }
+        set
+        {
+            _feedNotification = value;
+            OnPropertyChanged(nameof(FeedNotification));
+        }
+    }
+
     private FeedsViewModel _context;
 
     public FeedsViewModel Context
@@ -71,11 +84,32 @@ public class EditFeedViewModel : BaseViewModel
         await App.Current.Windows[0].Page.Navigation.PopAsync();
 
     });
+
+
+    public App CurrentApp { get; }
     public EditFeedViewModel(Feed feed, FeedsViewModel vm, GeneralDataBase generalDB)
     {
         _generalDB = generalDB;
         _feed = feed;
         _initialKeyWords = feed.Keywords;
         _context = vm;
+
+        CurrentApp = App.Current as App;
+
+        RefreshFeedSubStatus();
+
+    }
+    /// <summary>
+    /// Manually refresh the feed subscription status
+    /// </summary>
+    private async void RefreshFeedSubStatus ()
+    {
+        string id = _feed.MongoID;
+        string token = await SecureStorage.GetAsync(AppConstant.NotificationToken);
+        if (!string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(token))
+        {
+            FeedNotification = await CurrentApp.DataFetcher.CheckSubStatus(id, token);
+        }
+        return;
     }
 }
