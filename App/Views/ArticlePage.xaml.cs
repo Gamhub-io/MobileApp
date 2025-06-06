aca2c5c7ec9fd5002cfcb1142f0d5e5095a7fb32
bricefriha <contact@bricefriha.dev>
2025-06-06 13:50:29 +0100

    acticle page - store review | change the threshold

--- App/Views/ArticlePage.xaml.cs
+++ App/Views/ArticlePage.xaml.cs
@@ -8,59 +8,59 @@ using System.Diagnostics;
 
 namespace GamHubApp.Views;
 
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ArticlePage : ContentPage
-    {
-        private const string TimeSpentKey = "timeSpentOnArticles";
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ArticlePage : ContentPage
+{
+    private const string TimeSpentKey = "timeSpentOnArticles";
 #if DEBUG
 
         private const double TimeMaxArticles = 0;
         private const bool _isTest = true;
 #else
-        private const double TimeMaxArticles = 30;
+        private const double TimeMaxArticles = 10;
         private const bool _isTest = false;
 #endif
-        private ArticleViewModel _vm;
-        private uint _modalHeightStart = 0;
-        private uint _modalWidthStart = 50;
+    private ArticleViewModel _vm;
+    private uint _modalHeightStart = 0;
+    private uint _modalWidthStart = 50;
 
 
-        public ArticlePage(Article article)
-        {
-            InitializeComponent();
+    public ArticlePage(Article article)
+    {
+        InitializeComponent();
 
-            BindingContext = _vm = new ArticleViewModel(article);
-        }
+        BindingContext = _vm = new ArticleViewModel(article);
+    }
 
     protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
-        {
-            // Stop the timer
-            StopTimer();
+    {
+        // Stop the timer
+        StopTimer();
 
 
-            // Stop all text to speech
-            _vm.StopTtS();
-
+        // Stop all text to speech
+        _vm.StopTtS();
+        
         base.OnNavigatedFrom(args);
-             
 
-        }
-        /// <summary>
-        /// Stop the timer that count the time spent on reading article pages
-        /// </summary>
-        private async void StopTimer()
-        {
 
-            // Register the time spent in total
-            double timeSpentSoFar = Preferences.Get(TimeSpentKey, TimeSpan.Zero.TotalMinutes);
+    }
+    /// <summary>
+    /// Stop the timer that count the time spent on reading article pages
+    /// </summary>
+    private async void StopTimer()
+    {
 
-            // Stop the timer
-            _vm.TimeSpent.Stop();
+        // Register the time spent in total
+        double timeSpentSoFar = Preferences.Get(TimeSpentKey, TimeSpan.Zero.TotalMinutes);
 
-            double timeSpentOnArticles = timeSpentSoFar + _vm.TimeSpent.Elapsed.TotalMilliseconds;
+        // Stop the timer
+        _vm.TimeSpent.Stop();
 
-            // Save the Time spent
-            Preferences.Set(TimeSpentKey, timeSpentOnArticles);
+        double timeSpentOnArticles = timeSpentSoFar + _vm.TimeSpent.Elapsed.TotalMilliseconds;
+
+        // Save the Time spent
+        Preferences.Set(TimeSpentKey, timeSpentOnArticles);
 
         if (TimeSpan.FromMilliseconds(timeSpentOnArticles).TotalMinutes >= TimeMaxArticles
              && !Preferences.Get(AppConstant.ReviewAsked, false))
@@ -80,87 +80,87 @@ namespace GamHubApp.Views;
 
 
 
+    }
+
+    /// <summary>
+    /// Function to open a the dropdrown
+    /// </summary>
+    public void OpenDropdownMenu()
+    {
+        double height = 70;
+        double width = 180;
+
+        // Animation
+        void callbackH(double inputH) => dropdownMenu.HeightRequest = inputH;
+        void callbackW(double inputW) => dropdownMenu.WidthRequest = inputW;
+
+        uint rate = 24;
+        dropdownMenu.Animate("AnimHeightDropdownMenu", callbackH, dropdownMenu.Height, height, rate, 100, Easing.SinOut);
+        dropdownMenu.Animate("AnimWidthDropdownMenu", callbackW, dropdownMenu.Width, width, rate, 100, Easing.SinOut);
+        dropdownMenu.Padding = 3;
+
+        _vm.IsMenuOpen = true;
+    }
+    /// <summary>
+    /// Function to close a modal
+    /// </summary>
+    public void CloseDropdownMenu()
+    {
+
+        // Animation
+        void callbackH(double inputH) => dropdownMenu.HeightRequest = inputH;
+        void callbackW(double inputW) => dropdownMenu.WidthRequest = inputW;
+        uint rate = 24;
+
+        dropdownMenu.Animate("AnimWidthDropdownMenu", callbackW, dropdownMenu.Width, _modalWidthStart, rate, 500, Easing.SinOut);
+        dropdownMenu.Animate("AnimHeightDropdownMenu", callbackH, dropdownMenu.Height, _modalHeightStart, rate, 500, Easing.SinOut);
+
+        dropdownMenu.Padding = 0;
+
+        _vm.IsMenuOpen= false;
+    }
+
+    private void Menu_Clicked(object sender, EventArgs e)
+    {
+        // If dropdown is closed
+        if (dropdownMenu.Padding == 0)
+        {
+            OpenDropdownMenu();
+            return;
         }
 
-        /// <summary>
-        /// Function to open a the dropdrown
-        /// </summary>
-        public void OpenDropdownMenu()
+        CloseDropdownMenu();
+    }
+
+    private void MenuItem_Tapped(object sender, EventArgs e)
+    {
+        // If dropdown is open
+        if (dropdownMenu.Padding != 0)
         {
-            double height = 70;
-            double width = 180;
-
-            // Animation
-            void callbackH(double inputH) => dropdownMenu.HeightRequest = inputH;
-            void callbackW(double inputW) => dropdownMenu.WidthRequest = inputW;
-
-            uint rate = 24;
-            dropdownMenu.Animate("AnimHeightDropdownMenu", callbackH, dropdownMenu.Height, height, rate, 100, Easing.SinOut);
-            dropdownMenu.Animate("AnimWidthDropdownMenu", callbackW, dropdownMenu.Width, width, rate, 100, Easing.SinOut);
-            dropdownMenu.Padding = 3;
-
-            _vm.IsMenuOpen = true;
+            CloseDropdownMenu();
         }
-        /// <summary>
-        /// Function to close a modal
-        /// </summary>
-        public void CloseDropdownMenu()
+    }
+
+    private async void SwipeBackgroundDown_Swiped(object sender, SwipedEventArgs e)
+    {
+        // If dropdown is open
+        if (dropdownMenu.Padding != 0)
         {
-
-            // Animation
-            void callbackH(double inputH) => dropdownMenu.HeightRequest = inputH;
-            void callbackW(double inputW) => dropdownMenu.WidthRequest = inputW;
-            uint rate = 24;
-
-            dropdownMenu.Animate("AnimWidthDropdownMenu", callbackW, dropdownMenu.Width, _modalWidthStart, rate, 500, Easing.SinOut);
-            dropdownMenu.Animate("AnimHeightDropdownMenu", callbackH, dropdownMenu.Height, _modalHeightStart, rate, 500, Easing.SinOut);
-
-            dropdownMenu.Padding = 0;
-
-            _vm.IsMenuOpen= false;
-        }
-
-        private void Menu_Clicked(object sender, EventArgs e)
-        {
-            // If dropdown is closed
-            if (dropdownMenu.Padding == 0)
-            {
-                OpenDropdownMenu();
-                return;
-            }
-
             CloseDropdownMenu();
         }
 
-        private void MenuItem_Tapped(object sender, EventArgs e)
-        {
-            // If dropdown is open
-            if (dropdownMenu.Padding != 0)
-            {
-                CloseDropdownMenu();
-            }
-        }
-
-        private async void SwipeBackgroundDown_Swiped(object sender, SwipedEventArgs e)
-        {
-            // If dropdown is open
-            if (dropdownMenu.Padding != 0)
-            {
-                CloseDropdownMenu();
-            }
-
-            // If dropdown is open
-            await scrollview.ScrollToAsync(scrollview.ScrollX, scrollview.ScrollY - 5, true);
-        }
-
-        private async void SwipeBackgroundUp_Swiped(object sender, SwipedEventArgs e)
-        {
-            // If dropdown is open
-            if (dropdownMenu.Padding != 0)
-            {
-                CloseDropdownMenu();
-            }
-            // If dropdown is open
-            await scrollview.ScrollToAsync(scrollview.ScrollX, scrollview.ScrollY + 5, true);
-        }
+        // If dropdown is open
+        await scrollview.ScrollToAsync(scrollview.ScrollX, scrollview.ScrollY - 5, true);
     }
+
+    private async void SwipeBackgroundUp_Swiped(object sender, SwipedEventArgs e)
+    {
+        // If dropdown is open
+        if (dropdownMenu.Padding != 0)
+        {
+            CloseDropdownMenu();
+        }
+        // If dropdown is open
+        await scrollview.ScrollToAsync(scrollview.ScrollX, scrollview.ScrollY + 5, true);
+    }
+}
