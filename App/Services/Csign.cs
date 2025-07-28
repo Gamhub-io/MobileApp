e7da8078778e07517514eb627f5362a76cdc8a0f
bricefriha <contact@bricefriha.dev>
2025-07-28 12:54:27 +0100

    Fetcher - headers | sign a API key

---
+++ App/Services/Csign.cs
@@ -0,0 +1,17 @@
+﻿
+using GamHubApp.Core;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GamHubApp.Services;
+
+public class Csign
+{
+    public static string GenerateApiKey()
+    {
+        DateTime date = DateTime.Now;
+
+        using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(AppConstant.ShaSalt));
+        return Convert.ToHexStringLower(hmac.ComputeHash(Encoding.UTF8.GetBytes($"{{token:{AppConstant.MonitoringKey},d:{date.Day},m:{date.Month - 1},y:{date.Year},h:{date.Hour}}}")));
+    }
+}
