c6b41f4fb19476bbecc826ff5b33a9b51a3c7b6f
bricefriha <contact@bricefriha.dev>
2026-02-27 20:30:08 +0000

    fix deal tabbar colour

--- App/Views/DealsPage.xaml.cs
+++ App/Views/DealsPage.xaml.cs
@@ -20,6 +20,22 @@ public partial class DealsPage : ContentPage
         Preferences.Set(PreferencesKeys.NewDealCount, 0);
         (App.Current as App).ShowLoadingIndicator();
 
+        // Note: this is a workaround for a MAUI 10 bug that prevents the tabbar colours to set proper
+        Dispatcher.Dispatch(() =>
+        {
+            Shell.SetTabBarBackgroundColor(this,
+                (Color)Application.Current.Resources["LightDark"]);
+
+            Shell.SetTabBarForegroundColor(this,
+                (Color)Application.Current.Resources["Primary"]);
+
+            Shell.SetTabBarTitleColor(this,
+                (Color)Application.Current.Resources["Primary"]);
+
+            Shell.SetTabBarUnselectedColor(this,
+                (Color)Application.Current.Resources["UnselectedTabFont"]);
+        });
+
         Resume();
     }
     public void Resume()
