5472c0d60a90d038f27da3ffa7bf1864a4fa0a83
bricefriha <contact@bricefriha.dev>
2026-07-30 18:13:32 +0100

    preferences keys - remove the `SourceSelection `

--- App/Core/PreferencesKeys.cs
+++ App/Core/PreferencesKeys.cs
@@ -13,5 +13,4 @@ public class PreferencesKeys
     public const string DealFilterCode = "DEAL_FILTER_CODE";
     public const string LastDealVisit = "LAST_DEAL_VISIT";
     public const string CultureInfo = "CULTURE_INFO";
-    public const string SourceSelection = "SOURCE_SELECTION";
 }
