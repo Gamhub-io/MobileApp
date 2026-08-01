e7c1bbeb42087a3b758eea682a1d38f83a69cb15
Brice Friha <37577669+bricefriha@users.noreply.github.com>
2026-08-01 14:38:32 +0100

    Imp: increase trending period (#456)
    * fetcher - GetTrendingArticles | fix type cast
    
    * Fetcher - GetTrendingArticles | extend period to 18h

--- App/Services/Fetcher.cs
+++ App/Services/Fetcher.cs
@@ -429,20 +429,20 @@ public class Fetcher
             
             Dictionary<string,string> parameters = new ()
             {
-                { "period", "5h"},
+                { "period", "18h"},
                 { "limit", "5"},
             };
 
             Dictionary<string, string> headers = (Headers?.Count ?? 0) > 0 ? Headers : await GetHeaders();
 
-            var articleTrends = (await WithRetryAsync(() =>
-                 this.WebService.Get<ArticleTrendResponse>(controller: "monitor/trends",
+            var articleTrends = (await WithRetryAsync(async () =>
+                 await this.WebService.Get<ArticleTrendResponse>(controller: "monitor/trends",
                                                                parameters: parameters,
                                                                singleUseHeaders: headers,
                                                                jsonBody: null,
                                                                cancellationToken: cts.Token,
                                                                unSuccessCallback: e => _ = HandleHttpException(e))))?.Data.
-                                                               Select(at => at.Article);
+                                                               Select(at => at.Article).ToList();
 
             if (articleTrends == null)
                 return [];
